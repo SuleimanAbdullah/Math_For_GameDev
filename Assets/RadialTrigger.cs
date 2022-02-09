c42630cc718c171b8094f179ed70e36de1e1f814
Suleiman Abdullah <suleimanabdullahdev@gmail.com>
2022-02-09 13:55:35 +0300

    create new project

---
+++ Assets/RadialTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR 
+using UnityEditor;
+#endif
+public class RadialTrigger : MonoBehaviour
+{
+    public Transform objTf;
+
+    [Range(0f,4f)]
+    public float radius=1f;
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Vector2 objePos = transform.position;
+        Vector2 origin = objTf.position;
+
+        float dist = Vector2.Distance(objePos , origin);
+
+
+        bool isInside = dist < radius;
+
+        Handles.color = isInside ? Color.green : Color.red;
+        Handles.DrawWireDisc(objePos, new Vector3(0,0,1), radius);
+
+    }
+#endif
+}
---
+++ Assets/VectorMath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class VectorMath : MonoBehaviour
+{
+
+    public Transform aTfm;
+    public Transform bTfm;
+    [Range(0f, 4f)]
+    public float offset = 1f;
+    private void OnDrawGizmos()
+    {
+        Vector2 a = aTfm.position;
+        Vector2 b = bTfm.position;
+       
+        //Gizmos.DrawLine(a, b);
+        
+        Vector2 aTob = b - a;
+        Vector2 aTobdir = aTob.normalized;
+        Gizmos.DrawLine(a, a+aTobdir);
+        Vector2 vectorOffset = aTobdir* offset;
+        //Vector2 midPoint = (a + b / 2);
+        Gizmos.DrawSphere(a+vectorOffset, 0.05f);
+        //Vector2 point = transform.position;
+        //float length = point.magnitude;
+        //
+        //abDistance = Vector2.Distance(a, b);
+        //Vector2.Distance(a, b);
+        //(a - b).magnitude;
+        //Vector2 dirToPo = point.normalized;
+        // 
+
+    }
+}
+       
+        
+    
+
