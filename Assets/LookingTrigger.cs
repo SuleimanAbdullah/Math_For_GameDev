08482a146e5080f20fd64ce20ab460117166f17b
Suleiman Abdullah <suleimanabdullahdev@gmail.com>
2022-02-10 15:38:07 +0300

    Space Transformation local to world

---
+++ Assets/LookingTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class LookingTrigger : MonoBehaviour
+{
+    public Transform objTf;
+
+    [Range(0f,1f)]
+    public float precisness =0.5f;
+    private void OnDrawGizmos()
+    {
+        Vector2 center = transform.position;
+        Vector2 playerPos = objTf.position;
+        Vector2 playerLookDir = objTf.right;// x -axis
+
+        Vector2 playerToTriggerDir = (center - playerPos).normalized;
+
+        float lookness = Vector2.Dot(playerToTriggerDir, playerLookDir);
+
+        bool isLooking = lookness >= precisness;
+
+        Gizmos.color = isLooking ? Color.green : Color.red;
+        Gizmos.DrawLine(playerPos, playerPos + playerToTriggerDir);
+
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(playerPos, playerPos+ playerLookDir);
+    }
+}
---
+++ Assets/SpaceTransform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class SpaceTransform : MonoBehaviour
+{
+    public Vector2 localSpacePoint;
+    private void OnDrawGizmos()
+    {
+        Vector2 objPos = transform.position;
+        Vector2 right = transform.right;
+        Vector2 up = transform.up;
+
+        Vector2 LocalToWorld(Vector2 localPt)
+        {
+            Vector2 worldOffsets = localPt.x * right + localPt.y * up;
+            return (Vector2)transform.position + worldOffsets;
+        }
+
+        Vector2 worldSpacePoint = LocalToWorld(localSpacePoint);
+
+        DrawBasisVector(objPos, right, up);
+        DrawBasisVector(Vector2.zero, Vector2.right, Vector2.up);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(worldSpacePoint, 0.1f);
+    }
+
+    private void DrawBasisVector(Vector2 pos,Vector2 righ,Vector2 up)
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(pos, righ);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(pos, up);
+        Gizmos.color = Color.white;
+    }
+}
