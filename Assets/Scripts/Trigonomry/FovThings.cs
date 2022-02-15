0945fba1f383740d4b10281a27ccdd98b69a9235
Suleiman Abdullah <suleimanabdullahdev@gmail.com>
2022-02-15 11:46:42 +0300

    Field of view example using trigonometry atan

---
+++ Assets/Scripts/Trigonomry/FovThings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FovThings : MonoBehaviour
+{
+    public Transform obj;
+
+    private void OnDrawGizmos()
+    {
+        Camera cam = GetComponent<Camera>();
+        Vector2 objRelPos = obj.transform.position - cam.transform.position;
+
+        float opposite = objRelPos.y;
+        float adjacent = objRelPos.x;
+
+        float angRad = Mathf.Atan(opposite / adjacent);
+
+        cam.fieldOfView = 2 * angRad * Mathf.Rad2Deg;
+    }
+}
